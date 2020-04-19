9da134620e5f870781e7aca76bd5a96a51da5cad
Tina Maria Thomas <tinainink@gmail.com>
2020-04-18 21:50:16 -0400

    Tina | Add Navigation container

--- App.tsx
+++ App.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import Summary from './components/Summary';
 declare const global: {HermesInternal: null | {}};
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <>
-      <Summary/>
-    </>
+  <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Summary"
+            component={Summary}
+            options={{title: 'Summary Expense'}}
+          />
+        </Stack.Navigator>
+        <Stack.Screen name="Profile" component={Summary} />
+      </NavigationContainer>
   );
 };
 
