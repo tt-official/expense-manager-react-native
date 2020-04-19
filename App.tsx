f168ca6cfaa25102d460d1034459231f937dfac6
Tina Maria Thomas <tinainink@gmail.com>
2020-04-18 22:23:54 -0400

    Tina | Create sidebar

--- App.tsx
+++ App.tsx
@@ -2,22 +2,49 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
+import { Button, DrawerLayoutAndroid, Text, StyleSheet, View } from 'react-native';
 import Summary from './components/Summary';
+
 declare const global: {HermesInternal: null | {}};
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: "50px",
+    backgroundColor: "#ecf0f1",
+    padding: 8
+  },
+  navigationContainer: {
+    flex: 1,
+    paddingTop:  "50px",
+    backgroundColor: "#fff",
+    padding: 8
+  }
+});
+
+  const navigationView = (
+    <View >
+      <Text style={{ margin: 10, fontSize: 15 }}>I'm in the Drawer!</Text>
+    </View>
+  );
 const App = () => {
   return (
-  <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
+
+    <NavigationContainer>
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen
             name="Summary"
             component={Summary}
-            options={{title: 'Summary Expense'}}
+            options={{title: 'Expense Manager'}}
           />
-        </Stack.Navigator>
-        <Stack.Screen name="Profile" component={Summary} />
+           <Drawer.Screen name="Profile" component={Summary} />
+        </Drawer.Navigator>
       </NavigationContainer>
   );
 };
